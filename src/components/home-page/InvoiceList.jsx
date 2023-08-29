066e97433236161c1c26dbb0b022395ac80cd741
Victor Villazon <victorzn23@gmail.com>
2023-08-28 20:45:58 -0500

    trabajando en una fila de invoices

--- src/components/home-page/InvoiceList.jsx
+++ src/components/home-page/InvoiceList.jsx
@@ -1,6 +1,24 @@
+import { TbChevronRight } from "react-icons/tb"
+
 const InvoiceList = () => {
   return (
-    <div>InvoiceList</div>
+    <main className="w-[740px] m-auto">
+      <article className="text-white">
+        <div>
+          <span>#</span>
+          <span>RT3080</span>
+        </div>
+        <div>Due 19 Aug 2023</div>
+        <div>Jensen Huang</div>
+        <div>$ 1,800.90</div>
+        <div>
+          Paid
+        </div>
+        <div>
+          <TbChevronRight />
+        </div>
+      </article>
+    </main>
   )
 }
 
