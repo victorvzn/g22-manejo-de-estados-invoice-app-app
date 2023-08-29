7b0c2a745bc11780ff3c1ed40951bf7dfb945286
Victor Villazon <victorzn23@gmail.com>
2023-08-28 20:20:59 -0500

    añadiendo boton new invoice

--- src/components/home-page/Header.jsx
+++ src/components/home-page/Header.jsx
@@ -1,7 +1,14 @@
 const Header = () => {
   return (
-    <header className="w-[740px] mx-auto bg-green-200">
-      Header
+    <header className="w-[740px] mx-auto  bg-red-900 my-20 flex justify-between items-center">
+      <div className="text-white">
+        <h1 className="text-4xl font-extrabold">Invoices</h1>
+        <span>There are 7 total invoices</span>
+      </div>
+
+      <button className="bg-indigo-400 rounded-full px-5 py-3 font-extrabold text-white">
+        New Invoice
+      </button>
     </header>
   )
 }
