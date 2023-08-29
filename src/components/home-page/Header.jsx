0737771b95ecf06a1e52d25babfa052679f0ec63
Victor Villazon <victorzn23@gmail.com>
2023-08-28 20:30:41 -0500

    agregando icono de new invoice

--- src/components/home-page/Header.jsx
+++ src/components/home-page/Header.jsx
@@ -1,12 +1,17 @@
+import { TbSquareRoundedPlusFilled } from "react-icons/tb";
+
 const Header = () => {
   return (
-    <header className="w-[740px] mx-auto  bg-red-900 my-20 flex justify-between items-center">
+    <header className="w-[740px] mx-auto my-20 flex justify-between items-center">
       <div className="text-white">
-        <h1 className="text-4xl font-extrabold">Invoices</h1>
+        <h1 className="text-4xl font-medium">Invoices</h1>
         <span>There are 7 total invoices</span>
       </div>
 
-      <button className="bg-indigo-400 rounded-full px-5 py-3 font-extrabold text-white">
+      <button
+        className="bg-indigo-400 rounded-full px-5 py-3 font-semibold text-white flex justify-between items-center gap-4"
+      >
+        <TbSquareRoundedPlusFilled size="2rem" />
         New Invoice
       </button>
     </header>
