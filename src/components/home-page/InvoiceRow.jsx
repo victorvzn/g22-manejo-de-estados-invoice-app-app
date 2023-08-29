be437d69da9b2bdef73e0de0b6665433e5c72b02
Victor Villazon <victorzn23@gmail.com>
2023-08-28 23:09:21 -0500

    agregndo utilitario para formatear numeros

--- src/components/home-page/InvoiceRow.jsx
+++ src/components/home-page/InvoiceRow.jsx
@@ -2,6 +2,8 @@ import { TbChevronRight, TbCircleFilled } from "react-icons/tb"
 
 import BaseTag from "../shared/BaseTag"
 
+import { formatNumber } from "../../utils"
+
 const InvoiceRow = ({ invoice }) => {
   return (
     <article className="text-white flex justify-between items-center gap-3 bg-slate-700 rounded-lg px-4 py-6">
@@ -13,7 +15,7 @@ const InvoiceRow = ({ invoice }) => {
       <div className="w-40 font-bold">{invoice.bill.to.client.name}</div>
       <div className="w-44 text-3xl text-right">
         {invoice.invoice.currency.symbol}
-        {invoice.invoice.grandTotal}
+        {formatNumber(invoice.invoice.grandTotal)}
       </div>
       <div className="w-40 flex justify-center">
         {invoice.status === 'paid' &&
---
+++ src/utils/index.js
@@ -0,0 +1,9 @@
+// Intl.NumberFormat
+
+export const formatNumber = (number) => {
+  const locales = 'en-US'
+  const options = {
+    minimumFractionDigits: 2
+  }
+  return new Intl.NumberFormat(locales, options).format(number)
+}
